049671f45195776055145e1821460fe66dd7ed12
kv45 <kshitijverma852@gmail.com>
2025-07-15 04:37:31 +0100

    reformat with prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
-import { PrimeReactProvider } from 'primereact/api'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { PrimeReactProvider } from "primereact/api";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <PrimeReactProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </PrimeReactProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<PrimeReactProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</PrimeReactProvider>
+);
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 function About() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">Welcome to the About Page</h1>
-      <p className="mt-2">You can only see this by visiting <code>/about</code>.</p>
-    </div>
-  )
+	return (
+		<div className="p-6">
+			<h1 className="text-3xl font-bold">Welcome to the About Page</h1>
+			<p className="mt-2">
+				You can only see this by visiting <code>/about</code>.
+			</p>
+		</div>
+	);
 }
-export default About
+export default About;

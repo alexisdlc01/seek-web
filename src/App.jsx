9872be87887f5974aff433767105507173b72bdb
kv45 <kshitijverma852@gmail.com>
2025-07-15 04:52:15 +0100

    add basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/about" element={<About />} />
-		</Routes>
+		<>
+			<Navbar />
+			<Routes>
+				<Route />
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</>
 	);
 }
 

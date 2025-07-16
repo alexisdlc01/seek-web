0d01298c83a52228fe906ea688b64b5f3babb651
kv45 <kshitijverma852@gmail.com>
2025-07-16 12:30:32 +0100

    make navbar look decent and responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,7 @@ import { Button } from "primereact/button";
 export default function Navbar() {
 	const itemRenderer = item =>
 		item.label === "Login" ? (
-			<Button className={"mx-2 relative"} rounded>
-				<span className="mx-2">Login</span>
-			</Button>
+			<Button className={"pl-3 relative"}>Login</Button>
 		) : (
 			<a className="p-menuitem-link relative">
 				<span className="mx-2">{item.label}</span>
@@ -21,29 +19,34 @@ export default function Navbar() {
 		{ label: "Login", template: itemRenderer }
 	];
 
-	const start = (
-		<img
-			alt="logo"
-			src="https://primefaces.org/cdn/primereact/images/logo.png"
-			height="40"
-			className="mx-3 max-h-15"
-		></img>
-	);
-
 	return (
-		<div className="card">
+		<div className="relative w-full mt-4 mb-4 md:mt-0 md:mb-0">
 			<Menubar
-				start={start}
-				end={
-					<Menubar
-						model={items}
-						pt={{
-							root: {
-								className: "border-none"
-							}
-						}}
-					/>
-				}
+				model={items}
+				style={{
+					border: "none",
+					boxShadow: "none"
+				}}
+				pt={{
+					root: {
+						className: `
+							flex justify-center 
+							!border-none !shadow-none 
+							relative md:px-0 py-3
+						`
+					},
+					menu: {
+						className: "flex justify-center w-full"
+					},
+					button: {
+						className: `
+							absolute left-4 top-1/2 -translate-y-1/2 
+							md:static md:translate-y-0 md:ml-0
+							!border-none !shadow-none !bg-transparent !outline-none
+							hover:!bg-transparent focus:!ring-0 focus:!shadow-none
+						`
+					}
+				}}
 			/>
 		</div>
 	);

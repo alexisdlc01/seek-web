9ca69b467a4d2283d762be3d1c615a9226978585
kv45 <kshitijverma852@gmail.com>
2025-07-16 11:35:58 +0100

    align navbar items to right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,81 +1,24 @@
 import React from "react";
 import { Menubar } from "primereact/menubar";
-import { Badge } from "primereact/badge";
-import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 
 export default function Navbar() {
-	const itemRenderer = item => (
-		<a className="flex align-items-center p-menuitem-link">
-			<span className={item.icon} />
-			<span className="mx-2">{item.label}</span>
-			{item.badge && <Badge className="ml-auto" value={item.badge} />}
-			{item.shortcut && (
-				<span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">
-					{item.shortcut}
-				</span>
-			)}
-		</a>
-	);
+	const itemRenderer = item =>
+		item.label === "Login" ? (
+			<Button className={"mx-2 relative"} rounded>
+				<span className="mx-2">Login</span>
+			</Button>
+		) : (
+			<a className="p-menuitem-link relative">
+				<span className="mx-2">{item.label}</span>
+			</a>
+		);
 	const items = [
-		{
-			label: "Home",
-			icon: "pi pi-home"
-		},
-		{
-			label: "Features",
-			icon: "pi pi-star"
-		},
-		{
-			label: "Projects",
-			icon: "pi pi-search",
-			items: [
-				{
-					label: "Core",
-					icon: "pi pi-bolt",
-					shortcut: "⌘+S",
-					template: itemRenderer
-				},
-				{
-					label: "Blocks",
-					icon: "pi pi-server",
-					shortcut: "⌘+B",
-					template: itemRenderer
-				},
-				{
-					label: "UI Kit",
-					icon: "pi pi-pencil",
-					shortcut: "⌘+U",
-					template: itemRenderer
-				},
-				{
-					separator: true
-				},
-				{
-					label: "Templates",
-					icon: "pi pi-palette",
-					items: [
-						{
-							label: "Apollo",
-							icon: "pi pi-palette",
-							badge: 2,
-							template: itemRenderer
-						},
-						{
-							label: "Ultima",
-							icon: "pi pi-palette",
-							badge: 3,
-							template: itemRenderer
-						}
-					]
-				}
-			]
-		},
-		{
-			label: "Contact",
-			icon: "pi pi-envelope",
-			badge: 3,
-			template: itemRenderer
-		}
+		{ label: "Home", template: itemRenderer },
+		{ label: "About", template: itemRenderer },
+		{ label: "For Landlords", template: itemRenderer },
+		{ label: "Help", template: itemRenderer },
+		{ label: "Login", template: itemRenderer }
 	];
 
 	const start = (
@@ -86,19 +29,22 @@ export default function Navbar() {
 			className="mx-3 max-h-15"
 		></img>
 	);
-	const end = (
-		<div className="flex align-items-center gap-2">
-			<InputText
-				placeholder="Search"
-				type="text"
-				className="w-8rem sm:w-auto"
-			/>
-		</div>
-	);
 
 	return (
 		<div className="card">
-			<Menubar model={items} start={start} end={end} />
+			<Menubar
+				start={start}
+				end={
+					<Menubar
+						model={items}
+						pt={{
+							root: {
+								className: "border-none"
+							}
+						}}
+					/>
+				}
+			/>
 		</div>
 	);
 }

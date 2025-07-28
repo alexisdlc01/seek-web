c204662a1254d6c6f5da6a09ca1aac00c490f5ab
Graham Heathcote <grahamkheathcote@gmail.com>
2025-07-27 22:25:55 -0500

    changed time

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
 					initial={{ opacity: 0, y: 10 }}
 					animate={{ opacity: 1, y: 0 }}
 					exit={{ opacity: 0, y: 5 }}
-					transition={{ duration: 0.05 }}
+					transition={{ duration: .2 }}
 				>
 					<Routes location={location}>
 						<Route path="/" element={<Home />} />

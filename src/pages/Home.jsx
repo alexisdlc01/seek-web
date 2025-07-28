8c32b324225bd3cca53783593c158764b7cbc24a
Graham Heathcote <grahamkheathcote@gmail.com>
2025-07-28 00:15:29 -0500

    disable cast

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,6 +24,8 @@ export default function LandingPage() {
 					muted
 					playsInline
 					className="absolute top-0 left-0 w-full h-full object-cover z-0"
+					onContextMenu={e => e.preventDefault()}
+
 				>
 					<source src="/dummy_background.mp4" type="video/mp4" />
 					Your browser does not support the background video.

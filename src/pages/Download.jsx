0e760f19e95a7cca2568ce55a7cbf47608371fe0
Graham Heathcote <grahamkheathcote@gmail.com>
2025-09-04 21:26:38 -0500

    download

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -44,11 +44,11 @@ function CoreDiscovery() {
 				{/* Left: image placeholders (now positioned, not stacked) */}
 				<div className="relative mx-auto w-full max-w-md h-[320px] sm:h-[380px]">
 					<PhonePlaceholder
-						className="absolute -left-6 md:-left-10 top-0"
+						className="absolute -left-2 sm:-left-6 md:-left-10 top-0"
 						delay={0.0}
 					/>
 					<PhonePlaceholder
-						className="absolute -right-6 md:-right-10 top-0"
+						className="absolute -right-2 sm:-right-6 md:-right-10 top-0"
 						delay={0.0}
 					/>
 					<PhonePlaceholder
@@ -175,8 +175,8 @@ function ApplicationManagement() {
 
 				{/* Right: image placeholders */}
 				<div className="relative mx-auto w-full max-w-md h-[320px] sm:h-[380px]">
-					<PhonePlaceholder className="absolute -left-6 md:-left-10 top-0" />
-					<PhonePlaceholder className="absolute -right-6 md:-right-10 top-0" />
+					<PhonePlaceholder className="absolute -left-2 sm:-left-6 md:-left-10 top-0" />
+					<PhonePlaceholder className="absolute -right-2 sm:-right-6 md:-right-10 top-0" />
 					<PhonePlaceholder
 						className="absolute left-1/2 -translate-x-1/2 bottom-0"
 						delay={1.2}
@@ -193,8 +193,8 @@ function CommunicationSecurity() {
 			<div className="max-w-6xl mx-auto px-4 grid lg:grid-cols-2 gap-12 items-center">
 				{/* Left: image placeholders */}
 				<div className="relative mx-auto w-full max-w-md h-[320px] sm:h-[380px]">
-					<PhonePlaceholder className="absolute -left-6 md:-left-10 top-0" />
-					<PhonePlaceholder className="absolute -right-6 md:-right-10 top-0" />
+					<PhonePlaceholder className="absolute -left-2 sm:-left-6 md:-left-10 top-0" />
+					<PhonePlaceholder className="absolute -right-2 sm:-right-6 md:-right-10 top-0" />
 					<PhonePlaceholder
 						className="absolute left-1/2 -translate-x-1/2 bottom-0"
 						delay={1.2}

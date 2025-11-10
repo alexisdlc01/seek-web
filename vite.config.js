606f200ca7ba31dbe00932dbaeedae024ac7f2af
kv45 <kshitijverma852@gmail.com>
2025-11-10 10:31:58 +0000

    fix p tag hover

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	// server: {
-	// 	proxy: {
-	// 		'/api': {
-	// 			target: 'http://localhost:3000',
-	// 			changeOrigin: true,
-	// 			rewrite: path => path.replace(/^\/api/, ''),
-	// 		},
-	// 	},
-	// },
+	plugins: [react(), tailwindcss()]
 });

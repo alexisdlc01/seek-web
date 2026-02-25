7cf90b11d990ee3009853ac1004acd86eab419b0
Graham Heathcote <grahamkheathcote@gmail.com>
2026-02-25 00:53:02 +0000

    bathroom bump up

--- src/pages/AddListing.jsx
+++ src/pages/AddListing.jsx
@@ -66,7 +66,17 @@ const AddListing = () => {
 		{ label: "1", value: 1 },
 		{ label: "1.5", value: 1.5 },
 		{ label: "2", value: 2 },
-		{ label: "2.5", value: 2.5 }
+		{ label: "2.5", value: 2.5 },
+		{ label: "3", value: 3 },
+		{ label: "3.5", value: 3.5 },
+		{ label: "4", value: 4 },
+		{ label: "4.5", value: 4.5 },
+		{ label: "5", value: 5 },
+		{ label: "5.5", value: 5.5 },
+		{ label: "6", value: 6 },
+		{ label: "6.5", value: 6.5 },
+		{ label: "7", value: 7 },
+		{ label: "7.5", value: 7.5 }
 	];
 	const [bathrooms, setBathrooms] = useState(null);
 	const [description, setDescription] = useState("");
@@ -251,9 +261,7 @@ const AddListing = () => {
 			availableUntil: endAvailabilityDate,
 			registerOfTitleKey: registerOfTitleKeyFromBackend,
 			registrationNumber: registrationNumber,
-			furnishingStatus: furnishingStatus
-				? furnishingStatus
-				: null,
+			furnishingStatus: furnishingStatus ? furnishingStatus : null,
 			epcRating: epcRating,
 			amenities: amenities,
 			photos: allPhotosUrls,
